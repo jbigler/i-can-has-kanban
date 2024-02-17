a4d82b2d4681bd401fcd056a13ca6641c1131fb7
Jeff Bigler <63808+jbigler@users.noreply.github.com>
2024-02-17 08:33:02 +0000

    Create popup notifications
    Installed stimulus-notification.
    Used flowbite toast component.

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
@@ -8,3 +10,6 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.turbo.min.js"
 pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.8
 pin "sortablejs" # @1.15.2
+pin "stimulus-notification" # @2.2.0
+pin "hotkeys-js" # @3.13.7
+pin "stimulus-use" # @0.51.3
