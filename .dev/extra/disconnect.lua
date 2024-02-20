2d989459cd4f00e42a2381df3168ec0f0428f941
Jeff Bigler <63808+jbigler@users.noreply.github.com>
2024-02-20 10:09:08 +0700

    Remove .dev folder from repo

--- .dev/extra/disconnect.lua
+++
@@ -1,7 +0,0 @@
-vim.keymap.set('n', '<leader>q', function()
-  for _, ui in pairs(vim.api.nvim_list_uis()) do
-    if ui.chan and not ui.stdout_tty then
-      vim.fn.chanclose(ui.chan)
-    end
-  end
-end, { noremap = true })
